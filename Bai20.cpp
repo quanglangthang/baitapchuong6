1d214969ea8d685b99c40e53b73fa11da9cab893
Quang lang thang <63640626+quanglangthang@users.noreply.github.com>
2022-11-30 09:24:42 +0700

    Create Bai20.cpp

---
+++ Bai20.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int fact(int a) {
+	if (a == 0) {
+		return 1;
+	}
+	else {
+		return a * fact(a - 1);
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	cout << fact(n);
+}
