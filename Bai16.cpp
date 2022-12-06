cde8f9b2829b6d5a39a8270ed1301c02b594e3b8
Quang lang thang <63640626+quanglangthang@users.noreply.github.com>
2022-12-06 22:35:02 +0700

    Create Bai16.cpp

---
+++ Bai16.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm> // Thu vien dung __gcd => ucll
+using namespace std;
+
+int UCLL(int a, int b) {
+	if (a == b) {
+		return a;
+	}
+	else {
+		if (a > b) {
+			return UCLL(a-b,b);
+		}
+		else {
+			return UCLL(a,b-a);
+		}
+	}
+}
+
+int main() {
+	int a,b;
+	cin >> a >> b;
+	cout << UCLL(a,b);
+}
